8d63df962e9c8211c1162c8f4725e3e3a1db7a30
jagtapnn0509 <jagtapnn05@gmail.com>
2021-05-07 17:14:23 +0530

    Add : function to play single player and his position is 0

--- Sneak_Ladder_Game/Program.cs
+++ Sneak_Ladder_Game/Program.cs
@@ -7,6 +7,8 @@ namespace Sneak_Ladder_Game
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the sneak ladder Game");
+            int Player_1 = 0;
+            Console.WriteLine("Player 1 Position is " + Player_1);
         }
     }
 }
