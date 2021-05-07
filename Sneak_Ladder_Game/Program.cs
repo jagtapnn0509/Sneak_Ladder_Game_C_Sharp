7d5f9a2e8bbb1a946b95de470cd62f76aa3418fb
jagtapnn0509 <jagtapnn05@gmail.com>
2021-05-07 22:50:39 +0530

    Add : Function for multiple plyer played the game and Disply the winning player

--- Sneak_Ladder_Game/Program.cs
+++ Sneak_Ladder_Game/Program.cs
@@ -4,11 +4,23 @@ namespace Sneak_Ladder_Game
 {
     class Program
     {
+        static int flag = 0;
         public int Roll_Dice()
         {
             Random random = new Random();
             int Roll_Dice_Value = random.Next(1, 7);
             return Roll_Dice_Value;
+        }
+        public void Change_Player()
+        {
+            if (flag == 1)
+            {
+                flag = 0;
+            }
+            else
+            {
+                flag = 1;
+            }
         }
 
         public int Check_Option(int Roll_Dice_Value, int Player_1,int Privious_postion)
@@ -21,6 +33,7 @@ namespace Sneak_Ladder_Game
                 case 1:
                     Console.WriteLine("Player got Ladder");
                     Player_1 = Player_1 + Roll_Dice_Value;
+
                     break;
                 case 2:
                     Console.WriteLine("Player got Sneak");
@@ -29,9 +42,11 @@ namespace Sneak_Ladder_Game
                     {
                         Player_1 = 0;
                     }
+                    Change_Player();
                     break;
                 default:
                     Console.WriteLine("Player got No Play");
+                    Change_Player();
                     break;
             }
             return Player_1;
@@ -40,24 +55,57 @@ namespace Sneak_Ladder_Game
         {
             Console.WriteLine("Welcome to the sneak ladder Game");
             int Player_1 = 0;
-            int count = 0;
-            while (Player_1 < 100)
+            int Player_2 = 0;
+            int Player_1_count = 0;
+            int Player_2_count = 0;
+            int Roll_Dice_value;
+            while (Player_1 < 100 && Player_2 < 100)
             {
                 Console.WriteLine("---------------------------------------------------------------------------");
                 Program sneakladderobj = new Program();
-                int Roll_Dice_value = sneakladderobj.Roll_Dice();
-                Console.WriteLine("Roll Dice value is : " + Roll_Dice_value);
-                count = count + 1;
-                int Privious_postion = Player_1;
-                Player_1 = sneakladderobj.Check_Option(Roll_Dice_value, Player_1,Privious_postion);
-
-                if (Player_1 > 100)
+                if (flag == 0)
                 {
-                    Player_1 =  Privious_postion;
+                    Console.WriteLine("********Player One Played the game*******");
+                    Roll_Dice_value = sneakladderobj.Roll_Dice();
+                    Console.WriteLine("Roll Dice value is : " + Roll_Dice_value);
+                    Player_1_count = Player_1_count + 1;
+                    int Player1_Privious_postion = Player_1;
+                    Player_1 = sneakladderobj.Check_Option(Roll_Dice_value, Player_1, Player1_Privious_postion);
+                    if (Player_1 > 100)
+                    {
+                        Player_1 = Player1_Privious_postion;
+                    }
+                    
                 }
-                Console.WriteLine("Player Position is : " + Player_1);
+                else
+                {
+                    Console.WriteLine("********Player Two Played the game*******");
+                    Roll_Dice_value = sneakladderobj.Roll_Dice();
+                    Console.WriteLine("Roll Dice value is : " + Roll_Dice_value);
+                    Player_2_count = Player_2_count + 1;
+                    int Player2_Privious_postion = Player_2;
+                    Player_2 = sneakladderobj.Check_Option(Roll_Dice_value, Player_2, Player2_Privious_postion);
+                    if (Player_2 > 100)
+                    {
+                        Player_2 = Player2_Privious_postion;
+                    }
+                    
+                }
+                Console.WriteLine("---------------------------------------------------------------------------");
+                Console.WriteLine("Player1 Position is : " + Player_1);
+                Console.WriteLine("Player2 Position is : " + Player_2);
             }
-            Console.WriteLine("No of times dice roll: " + count);
+            Console.WriteLine("---------------------------------------------------------------------------");
+            Console.WriteLine("No of times dice roll for player 1: " + Player_1_count);
+            Console.WriteLine("No of times dice roll for player 2: " + Player_2_count);
+            if ( Player_1 == 100)
+            {
+                Console.WriteLine("Player 1 Won the game");
+            }
+            else
+            {
+                Console.WriteLine("Player 2 Won the game");
+            }
         }
     }
 }
