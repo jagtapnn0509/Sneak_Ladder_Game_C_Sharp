f63bc87d05e57eba9a405d284d977d48b6a0d2a7
jagtapnn0509 <jagtapnn05@gmail.com>
2021-05-07 17:09:31 +0530

    Add : Welcome Message

---
+++ Sneak_Ladder_Game/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sneak_Ladder_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the sneak ladder Game");
+        }
+    }
+}
